597fe8f01852d2a4e1e9ce894bb42324e4e02141
Prince George <97896628+Billydbeast@users.noreply.github.com>
2024-05-02 08:05:20 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -3,16 +3,16 @@ const chalk = require('chalk');
 require('dotenv').config();
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "919931122319"],
-global.ownername = process.env.OWNER_NAME || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = [ process.env.OWNER_NUMBER || "08141934607"],
+global.ownername = process.env.OWNER_NAME || "Billydbeast",
+global.ytname = "Prince George"
+global.socialm = "Twitter: @Ozehdan"
+global.location = "Nigeria"
 
-global.botname = process.env.BOTNAME || "𝐌𝐀𝐑𝐈𝐀-𝐌𝐃",//name of the bot
+global.botname = process.env.BOTNAME || "Chigozie",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
+global.stickername = process.env.STICKER || "Ugo",
 global.packname = 'Sticker By'
 global.author = process.env.AUTHOR || "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰",
 //console view/theme
